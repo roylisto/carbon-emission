d34a2ec823403b2b48a613bb81fc16d972e5871e
Roy Pradana <33563774+roylisto@users.noreply.github.com>
2024-04-22 19:48:11 +0700

    feat: optimize flight route controller

--- app/Http/Controllers/Api/FlightRouteController.php
+++ app/Http/Controllers/Api/FlightRouteController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 use App\Http\Resources\FlightRouteResource;
 use App\Models\Emission;
 use App\Models\FlightRoute;
 use App\Services\Squake;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
 
 class FlightRouteController extends Controller
 {
@@ -26,75 +26,73 @@ class FlightRouteController extends Controller
         $input = $request->all();
 
         $validator = Validator::make($input, [
-            '*.origin' => 'required|string|max:3',
-            '*.destination' => 'required|string|max:3',
+            '*.origin' => 'required|string|size:3',
+            '*.destination' => 'required|string|size:3',
+            '*.number_of_travelers' => 'required|integer|min:1'
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        $outputItems = [];
-        $checkOnSquake = [];
+        $dataToCheck = [];
         $totalCarbonQuantity = 0;
+        $outputItems = [];
+
+        // Prepare data for DB and Squake calls in one loop
         foreach ($input as $key => $item) {
             $item['type'] = 'flight';
+            $dataToCheck[$key] = $item;
 
-            // find data in db
             $routeEmission = FlightRoute::findByOriginDestinationMethodology(
                 $item['origin'],
                 $item['destination'],
                 isset($item['methodology']) ? $item['methodology'] : 'MYCLIMATE'
             );
 
-            if (empty($routeEmission)) {
-                array_push($checkOnSquake, $item);
-            } else {
+            if (!empty($routeEmission)) {
                 $routeEmission['emission']['carbon_quantity'] = $routeEmission['emission']['carbon_quantity'] * $item['number_of_travelers'];
                 $totalCarbonQuantity += $routeEmission['emission']['carbon_quantity'];
-                array_push($outputItems, $routeEmission);
+                $outputItems[] = $routeEmission;
+                unset($dataToCheck[$key]); // Remove processed items from Squake check list
             }
         }
 
-        // if some data not exist in DB then call squake API
-        if (!empty($checkOnSquake)) {
+        // Call Squake API only if there's data left to check
+        if (!empty($dataToCheck)) {
             $squakeResponse = $this->squake->calculate([
                 'expand' => ['items'],
-                'items' => $checkOnSquake
+                'items' => array_values($dataToCheck), // Ensure proper indexing
             ]);
 
             if (isset($squakeResponse['errors'])) {
                 return new FlightRouteResource(false, 'Flight Emission Calculation ', $squakeResponse['errors']);
             }
 
-            // save new flight route emission
-            foreach ($squakeResponse['items'] as $key => $item) {
+            // Process Squake response and save data
+            try {
+                DB::beginTransaction();
+                foreach ($squakeResponse['items'] as $key => $item) {
+                    $check = $dataToCheck[$key];
+                    $item['carbon_quantity'] = (int) ($item['carbon_quantity'] / $check['number_of_travelers']);
 
-                $check = $checkOnSquake[$key];
-                $item['carbon_quantity'] = (int) ($item['carbon_quantity'] / $check['number_of_travelers']);
-
-                try {
-                    DB::beginTransaction();
-                    // store to emission table
                     $emission = Emission::create($item);
-
-                    // store to flight route
                     $insertData = FlightRoute::create([
                         'origin' => strtoupper($check['origin']),
                         'destination' => strtoupper($check['destination']),
                         'methodology' => isset($check['methodology']) ? strtoupper($check['methodology']) : 'MYCLIMATE',
                         'emission_id' => $emission->id,
                     ])->load('emission');
-                    DB::commit();
 
                     $newRouteEmission = FlightRoute::with('emission')->find($insertData->id);
                     $newRouteEmission['emission']['carbon_quantity'] = $newRouteEmission['emission']['carbon_quantity'] * $check['number_of_travelers'];
                     $totalCarbonQuantity += $newRouteEmission['emission']['carbon_quantity'];
-                    array_push($outputItems, $newRouteEmission);
-                } catch (\Exception $e) {
-                    DB::rollBack();
-                    return new FlightRouteResource(false, 'Flight Emission Calculation', $e->getMessage());
+                    $outputItems[] = $newRouteEmission;
                 }
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return new FlightRouteResource(false, 'Flight Emission Calculation', $e->getMessage());
             }
         }
 
